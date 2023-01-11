027ffabbb536a19aae5f80182d0f5e144746e5c9
IidaPeltonen <n0peii00@students.oamk.fi>
2023-01-11 20:54:58 +0200

    alku

---
+++ App.js
@@ -0,0 +1,31 @@
+// App.js
+import * as React from "react";
+import { View, Text } from "react-native";
+import "react-native-gesture-handler"; //this should be the first import in your code
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./components/Homescreen";
+import AboutScreen from "./components/Aboutscreen";
+import ContactScreen from './components/ContactScreen';
+
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+<Drawer.Navigator initialRouteName="Home">
+  <Stack.Screen name="Home" component={HomeScreen} />
+  <Stack.Screen name="About" component={AboutScreen} />
+  <Stack.Screen name="Contact" component={ContactScreen} />
+</Drawer.Navigator>
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+        <Stack.Screen name="Contact" component={ContactScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
