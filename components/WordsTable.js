871c3d8b35b48f8b549ce6e2a1ccc3ed9fd4baf2
IidaPeltonen <n0peii00@students.oamk.fi>
2023-01-15 19:48:04 +0200

    timer ei toimi

--- components/WordsTable.js
+++ components/WordsTable.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
-import { diff } from 'react-native-reanimated';
+import Timer from './Timer';
 import styles from "../styles/styles";
 
 //tarvitaan lista sanoja
@@ -9,11 +9,15 @@ const words2 = ['kissa', 'koira', 'koulu', 'kaali', 'sukka', 'talvi', 'silta']
 const words3 = ['apina', 'saavutus', 'kolina', 'hevonen', 'porkkana', 'peruna', 'kaappi', 'sitruuna']
 
 export default function WordsTable({ difficulty, time }) {
-    const [timer, setTimer] = useState(time) //kauanko sana näkyy, määräytyy vaikeustason mukaan
+
+    let fixedTime = (Number(time) * 1000)
+    const [timer, setTimer] = useState(fixedTime) //kauanko sana näkyy, määräytyy vaikeustason mukaan
     const [words, setWords] = useState([])
     const [isLoading, setIsLoading] = useState(false); // jotta useEffect toimii
     const [showable, setShowable] = useState(false); // näytettävä sana
 
+   
+    console.log('fixedTime: ' + (fixedTime+1))
 
     //haetaan sopivan tason sanat
     function getSelectedLvlWords() {
@@ -43,17 +47,22 @@ export default function WordsTable({ difficulty, time }) {
         //tarkistaa saatujen sanojen pituuden
         let length = (words.length - 1)
 
+/*         function showOne(showable) {
+            showOne(showable)
+            // tämän pitäisi kestää niin kauan kuin on aikaa
+            setShowable(showable)
+            setTimeout(() => {
+                setShowable('')
+            }, fixedTime)
+        } */
+
         //arpoo numeron väliltä 0-pituus, kunnes laskuri on 0
         for (let usedWords = 0; usedWords <= length; usedWords++) {
             let random = Math.floor(Math.random() * (words.length - 1))
-            console.log('sana: ' + words[random])
-            // tämän pitäisi kestää niin kauan kuin on aikaa
-            setShowable(words[random])
+            let randomWord = words[random]       
+            //showOne(randomWord)
+            setShowable(randomWord)
             words.splice(random, 1)
-            //jos sanay loppui, piiloitetaan vika ruudulta
-            if (words.length === 0) {
-                setShowable('')
-            }
             console.log('words: ' + words)
         }
     }
@@ -66,6 +75,7 @@ export default function WordsTable({ difficulty, time }) {
 
     return (
         <View style={styles.WordsTable}>
+            {/* <Timer word={showable} time={time}/> */}
             <Text style={styles.show}>{showable}</Text>
             <Text style={styles.Clock}>Aikaa jäljellä : {time}s </Text>
         </View>
